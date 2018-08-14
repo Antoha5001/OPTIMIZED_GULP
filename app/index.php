9347de0e174cf9bd8425be13ee458b2351b5a22a
Antoha5001 <plantsvzzombi@mail.ru>
2018-08-14 16:05:47 +0700

    gulp 4

---
+++ app/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>Document</title>
+    
+	<link rel="stylesheet" href="common/css/style.min.css"/>
+</head>
+<body>
+	<h1>12345</h1>	
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+	 Error fuga iste dolores temporibus voluptate quam alias, <br> ipsam autem, 
+	 ipsum at cupiditate explicabo sunt unde minima nihil blanditiis aut odit adipisci.</p>
+     <script src="common/script/common.min.js"></script>
+     <div>123</div>
+	 
+</body>
+</html>
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,68 @@
-var gulp 				= require('gulp'),
-sass 				= require('gulp-sass'),
-		browserSync = require('browser-sync'),
-		concat 			= require('gulp-concat'),
-		uglifyJs 		= require('gulp-uglifyjs'),
-		cssnano			= require('gulp-cssnano'),
-		rename			= require('gulp-rename'),  
-		del					= require('del'),  
-		imagemin		= require('gulp-imagemin'),  
-		pngquant		= require('imagemin-pngquant'),  
-		cache				= require('gulp-cache'),  
-		autoprefixer				= require('gulp-autoprefixer');  
+"use strict";
 
-		const srv = 'optimized-gulp.web:82';
+const gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync').create(),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	cssnano = require('gulp-cssnano'),
+	rename = require('gulp-rename'),
+	del = require('del'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant'),
+	cache = require('gulp-cache'),
+	autoprefixer = require('gulp-autoprefixer'),
+	pump = require('pump'),
+	babel = require('gulp-babel');
+	
 
-function defaultTask() {
-  
-	gulp.task('browser-sync',  function () {
-		browserSync.init({
-			/*server: {
-				baseDir: '500303_GULP'
-			},*/
-			proxy:srv,
-			notify: false
-			// tunnel: true,
-			// tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
-		});
-		browserSync.watch('app',browserSync.reload);
-	});
-  
+const srv = 'optimized-gulp.web:82';
+
+var myScripts = [
+	"./app/script/my.js",
+	"./app/script/my2.js",
+]
+
+
+function scripts() {
+
+	return gulp.src(myScripts)
+		.pipe(concat('common.min.js'))
+		.pipe(babel({
+            presets: ['env']
+        }))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/common/script'));
 }
 
+function styles(){
+	return gulp.src('./app/scss/style.scss')
+	.pipe(sass())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('app/common/css'));
+}
+
+
+function defaultTask() {
+
+
+}
+
+gulp.task('serve', function(){
+
+	browserSync.init({
+        proxy: "optimized-gulp.web:82"
+    });
+	
+	gulp.watch('app/scss/**/*.scss',gulp.series('style')).on('change', browserSync.reload);
+	gulp.watch('app/script/**/*.js', gulp.series('script')).on('change', browserSync.reload);
+
+	gulp.watch('app/**/*.php').on('change', browserSync.reload);
+	gulp.watch('app/**/*.html').on('change', browserSync.reload);
+});
+exports.script = scripts;
+exports.style = styles;
 exports.default = defaultTask;
 
+// gulp.task('script', scripts);
 
-
-gulp.task('watch', function () {
-
-        gulp.watch('app/scss/**/*.scss',gulp.series('sass'));
-        gulp.watch('app/script/**/*.js', gulp.series('js'));
-
-        gulp.watch('app/**/*.php').on('change', browserSync.reload);
-        gulp.watch('app/**/*.html').on('change', browserSync.reload);
-    });
-
-// gulp.task('default', gulp.parallel('watch','browser-sync'));
